36c3d793068fd66afcf132bf1361069a3b0f6a6d
Sam Cutler <sam.cutler@gmail.com>
2020-11-13 13:35:36 -0800

    core functionality working well, including tastedive API fetches, generating results list, and including links to functional Alibris and Indiebound search results

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 'use strict';
 
-// Define the API key.
+// Define the API keys.
 const apiKey = '391102-BookReco-Z2ZG9UZJ';
 
 // Define the base endpoint.
 const baseEndpoint = 'https://tastedive.com/api/similar?';
 
-// Define the proxy URL, to fix the header problem...
-const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+
+// Create the HTML string for each item in the results list
+function createResultsListItemString(resultObject) {
+    //console.log('Ran createResultsListItemString function.')
+    console.log(resultObject.Name);
+
+    const searchUrlTargetString = resultObject.Name.replace(/ /g, '+');
+
+    return `
+        <li>${resultObject.Name}
+            <ul>
+                <li>Teaser here.</li>
+                <li>Search for used books <a href='https://www.alibris.com/booksearch?mtype=B&keyword=${searchUrlTargetString}' target='_blank'>here</a>.</li>
+                <li>Search for books available at local bookstores <a href='https://www.indiebound.org/search/book?keys=${searchUrlTargetString}' target='_blank'>here</a>.</li>
+            </ul>
+        </li>`;
+}
+
+
+// Create Results List + insert into the DOM.
+function displayGoodResults(resultsArray) {
+    console.log('Ran displayGoodResults function.');
+
+    let resultsListHtmlString = '';
+
+    for (let i=0 ; i<resultsArray.length ; i++) {
+        resultsListHtmlString += createResultsListItemString(resultsArray[i]);
+    };
+
+    $('.js-results-list').append(resultsListHtmlString);
+
+    $('.results').removeClass('hidden');
+}
+
+
+
+// Logic to determine how to handle the search results
+function handleResults(responseJson) {
+    console.log('Ran handleResults function.');
+    console.log(responseJson);
+
+    // if don't get any results
+    if (responseJson.Similar.Results.length === 0) {
+        console.log('Did not get any search results. Need to update an error message element.');
+    } else {
+        console.log('Got search results!');
+        displayGoodResults(responseJson.Similar.Results);
+    }
+
+    // if do get search results
+}
+
+
 
 // Create the query string for the GET request.
 
@@ -32,13 +83,12 @@ function formatQueryParams(queryParams) {
     queryString += '&info=1';
 
     // Add the always-present type, callback (to get JSONP response), and key.
-    queryString += '&type=book'
+    //queryString += '&type=book'
     queryString += '&callback'
     queryString += queryParams.key
 
     return queryString;
 }
-
 
 // Submit the core GET request.
 function getRecommendations(requestedReferencesArray) {
@@ -53,12 +103,6 @@ function getRecommendations(requestedReferencesArray) {
     const URLtoBeFetched = baseEndpoint+queryString;
     console.log(URLtoBeFetched);
 
-    const options = {
-        headers: new Headers({
-            "Access-Control-Allow-Origin":'*'
-        })
-    };
-
     fetchJsonp(URLtoBeFetched)
         .then(response => {
             if (response.ok) {
@@ -67,8 +111,7 @@ function getRecommendations(requestedReferencesArray) {
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => console.log(responseJson))
-        //.then(responseJson => displayResults(responseJson))
+        .then(responseJson => handleResults(responseJson))
         .catch(err => {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
